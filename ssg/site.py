6660dd9360cb3cb8a1879dc8e2470a0d35f9bc45
mindofbrandon <zunigabrandon12@gmail.com>
2022-07-22 11:36:58 -0400

    adding self inside funcs

--- ssg/site.py
+++ ssg/site.py
@@ -16,16 +16,16 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                run_parser(path)
+                self.run_parser(path)
             
 
     def load_parser(self, extension):
         for parser in self.parsers:
-            if extenstion.valid_extension():
+            if parser.valid_extension(extension):
                 return parser
 
     def run_parser(self, path):
-        parser = load_parser(path.suffix)
+        parser = self.load_parser(path.suffix)
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
