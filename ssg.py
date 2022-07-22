1515617f512039443ec7967be7b6c1a485968f2c
mindofbrandon <zunigabrandon12@gmail.com>
2022-07-22 14:32:35 -0400

    parser subclass step6-10

--- ssg.py
+++ ssg.py
@@ -3,6 +3,6 @@ from ssg.site import Site
 import ssg.parsers
 
 def main(source="content", dest="dist"):
-    config = {"source":source, "dest":dest, "parsers":[ssg.parsers.ResourceParser(),],}
+    config = {"source":source, "dest":dest, "parsers":[ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()],}
     Site (**config).build()
     typer.run(main)
