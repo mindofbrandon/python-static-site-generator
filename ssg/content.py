039ea1d2a31b59ddff4ae7a412b5e5163758fe12
mindofbrandon <zunigabrandon12@gmail.com>
2022-07-22 12:11:52 -0400

    bugfixing conent: 1-3

--- ssg/content.py
+++ ssg/content.py
@@ -7,7 +7,7 @@ class Content(Mapping):
     __delimeter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
     
-    def load(self, cls, string): # make it with appropriate decorator?
-        self.__regex.split(string) # add a depth of 2?
-        load(fm, Loader)
+    def load(cls, string): # make it with appropriate decorator?
+        _, fm, content = cls.__regex.split(string, 2) # add a depth of 2
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
