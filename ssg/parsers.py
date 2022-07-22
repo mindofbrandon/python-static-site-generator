10413dabde6eb130f75a78f137248e49521a3ad3
mindofbrandon <zunigabrandon12@gmail.com>
2022-07-22 14:19:37 -0400

    bugfix md step 4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -37,11 +37,10 @@ class ResourceParser(Parser): # subclass of parser called resourcerparser
 class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
-    def parse(self, path, source, dest): 
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = markdown(content.body)
-        self.write(path, dest, html) # write html to path at dest?
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content) # changes string output to color gree
-
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
