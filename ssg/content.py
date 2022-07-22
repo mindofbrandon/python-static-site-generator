663d8a63061945ebd18176ed3da8b4070e99a33e
mindofbrandon <zunigabrandon12@gmail.com>
2022-07-22 12:28:41 -0400

    content: step4

--- ssg/content.py
+++ ssg/content.py
@@ -7,7 +7,12 @@ class Content(Mapping):
     __delimiter = r'^(?:-|\+){3}\s*$'
     __regex = re.compile(__delimiter, re.MULTILINE)
     
+    @classmethod # is this the decorator?
     def load(cls, string): # make it with appropriate decorator?
         _, fm, content = cls.__regex.split(string, 2) # add a depth of 2
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
